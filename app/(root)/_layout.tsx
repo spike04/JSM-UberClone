cee9b2d2cb2fc57f2b5c7e421361e65d1986cd0a
spike04 <rubinbajracharya@yahoo.com>
2024-08-21 13:41:54 +0545

    onboarding, sign-in, sign-up design done.

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const OnboardingLayout = () => {
+  return (
+    <View>
+      <Text>OnboardingLayout</Text>
+    </View>
+  )
+}
+
+export default OnboardingLayout
